fdd591f37feee8b7f406edb05cf7687d9c1cd8a5
smalaca <ab13.krakow@gmail.com>
2021-11-20 10:56:27 +0100

    fix persistency of Product entity

--- src/main/java/com/smalaca/jpa/JpaHelloWorld.java
+++ src/main/java/com/smalaca/jpa/JpaHelloWorld.java
@@ -10,10 +10,26 @@ import java.util.UUID;
 public class JpaHelloWorld {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ToDo");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
 
-        entityManager.persist(new Product(UUID.randomUUID(), "Water", "The best thing to drink"));
+        UUID id = UUID.randomUUID();
+        Product product = new Product(id, "Water", "The best thing to drink");
+        entityManager1.getTransaction().begin();
+        entityManager1.persist(product);
+        entityManager1.getTransaction().commit();
 
-        entityManager.close();
+        Product found = entityManager1.find(Product.class, id);
+        System.out.println(found);
+        entityManager1.close();
+
+        System.out.println("-----------------------------");
+        System.out.println("-------- NEW CONTEXT --------");
+        System.out.println("-----------------------------");
+
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+
+        System.out.println(entityManager2.find(Product.class, id));
+
+        entityManager2.close();
     }
 }
