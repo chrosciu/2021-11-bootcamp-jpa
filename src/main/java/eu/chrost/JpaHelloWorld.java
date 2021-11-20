7d39fc411c82b1d4c56e1ceb56918ddd3908ab8d
smalaca <ab13.krakow@gmail.com>
2021-11-20 11:02:43 +0100

    Added runWithEntityManager utility method

--- src/main/java/eu/chrost/JpaHelloWorld.java
+++ src/main/java/eu/chrost/JpaHelloWorld.java
@@ -4,27 +4,36 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 public class JpaHelloWorld {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ToDo");
-        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
-
         UUID id = UUID.randomUUID();
-        ProductRepository productRepository = new ProductRepository(entityManager1);
 
-        productRepository.save(new Product(id, "Water", "The best thing to drink"));
+        runWithEntityManager(entityManagerFactory, entityManager -> {
+            ProductRepository productRepository = new ProductRepository(entityManager);
+            productRepository.save(new Product(id, "Water", "The best thing to drink"));
 
-        Product found = productRepository.findById(id);
-        System.out.println(found);
+            Product found = productRepository.findById(id);
+            System.out.println(found);
+        });
 
-        entityManager1.close();
+        runWithEntityManager(entityManagerFactory, entityManager -> {
+            ProductRepository productRepository = new ProductRepository(entityManager);
+            System.out.println(productRepository.findById(id));
+        });
+    }
 
-        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
-        productRepository = new ProductRepository(entityManager2);
-
-        System.out.println(productRepository.findById(id));
-
-        entityManager2.close();
+    private static void runWithEntityManager(EntityManagerFactory entityManagerFactory, Consumer<EntityManager> action) {
+        EntityManager entityManager = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            action.accept(entityManager);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
     }
 }
