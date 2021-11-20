0aec1b50d3f00ab004d9e32c64ed51f5767ac46c
smalaca <ab13.krakow@gmail.com>
2021-11-20 11:12:38 +0100

    add many ToDo

--- src/main/java/com/smalaca/jpa/JpaHelloWorld.java
+++ src/main/java/com/smalaca/jpa/JpaHelloWorld.java
@@ -11,24 +11,25 @@ import java.util.UUID;
 public class JpaHelloWorld {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ToDo");
-        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
-        ToDoRepository toDoRepository = new ToDoRepository(entityManager1);
+        EntityManager context1 = entityManagerFactory.createEntityManager();
+        ToDoRepository toDoRepository = new ToDoRepository(context1);
 
-        UUID id = UUID.randomUUID();
-        toDoRepository.save(new ToDo(id, "conduct a training"));
+        toDoRepository.save(new ToDo(UUID.randomUUID(), "eat lunch"));
+        toDoRepository.save(new ToDo(UUID.randomUUID(), "conduct a training"));
+        toDoRepository.save(new ToDo(UUID.randomUUID(), "go to sleep"));
 
-        entityManager1.close();
+        context1.close();
 
         System.out.println("------------------------");
         System.out.println("------------------------");
         System.out.println("------------------------");
 
-        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
-        toDoRepository = new ToDoRepository(entityManager2);
+        EntityManager lastContext = entityManagerFactory.createEntityManager();
+        toDoRepository = new ToDoRepository(lastContext);
 
         toDoRepository.findAll()
                 .forEach(System.out::println);
 
-        entityManager2.close();
+        lastContext.close();
     }
 }
