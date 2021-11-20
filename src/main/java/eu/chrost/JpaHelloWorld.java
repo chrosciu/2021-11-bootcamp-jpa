71986efd298a9765182b1988bee951f67f3d9d4d
smalaca <ab13.krakow@gmail.com>
2021-11-20 10:16:46 +0100

    Introduce Product entity
    (cherry picked from commit 21f9445af243037c63ba56e46105975445d98e86)

--- src/main/java/eu/chrost/JpaHelloWorld.java
+++ src/main/java/eu/chrost/JpaHelloWorld.java
@@ -1,7 +1,19 @@
 package eu.chrost;
 
+import com.smalaca.jpa.domain.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.UUID;
+
 public class JpaHelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello JPA World");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ToDo");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.persist(new Product(UUID.randomUUID(), "Water", "The best thing to drink"));
+
+        entityManager.close();
     }
 }
