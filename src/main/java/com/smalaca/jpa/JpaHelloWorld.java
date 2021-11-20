392fe02aea1023c0ae105c706f851f194d85ac3a
smalaca <ab13.krakow@gmail.com>
2021-11-20 10:42:26 +0100

    Persist ToDo Item

--- src/main/java/com/smalaca/jpa/JpaHelloWorld.java
+++ src/main/java/com/smalaca/jpa/JpaHelloWorld.java
@@ -10,10 +10,26 @@ import java.util.UUID;
 public class JpaHelloWorld {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ToDo");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        
-        entityManager.persist(new ToDo(UUID.randomUUID(), "conduct a training"));
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
 
-        entityManager.close();
+        entityManager1.getTransaction().begin();
+        UUID id = UUID.randomUUID();
+        entityManager1.persist(new ToDo(id, "conduct a training"));
+
+        ToDo toDo = entityManager1.find(ToDo.class, id);
+
+        System.out.println(toDo);
+        entityManager1.getTransaction().commit();
+        entityManager1.close();
+
+        System.out.println("------------------------");
+        System.out.println("------------------------");
+        System.out.println("------------------------");
+
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+        ToDo found = entityManager2.find(ToDo.class, id);
+
+        System.out.println(found);
+        entityManager2.close();
     }
 }
--- src/main/java/com/smalaca/jpa/domain/ToDo.java
+++ src/main/java/com/smalaca/jpa/domain/ToDo.java
@@ -1,10 +1,16 @@
 package com.smalaca.jpa.domain;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.UUID;
 
+@ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
 public class ToDo {
     @Id
