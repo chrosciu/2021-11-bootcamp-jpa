7fdac987fe8a10919833abdb9f82a5e8bfa35bfa
smalaca <ab13.krakow@gmail.com>
2021-11-20 11:28:48 +0100

    remove Product and find all products

--- src/main/java/com/smalaca/jpa/JpaHelloWorld.java
+++ src/main/java/com/smalaca/jpa/JpaHelloWorld.java
@@ -6,32 +6,46 @@ import com.smalaca.jpa.domain.ProductRepository;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 import java.util.UUID;
 
 public class JpaHelloWorld {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ToDo");
-        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        EntityManager context1 = entityManagerFactory.createEntityManager();
 
-        UUID id = UUID.randomUUID();
-        ProductRepository productRepository = new ProductRepository(entityManager1);
+        ProductRepository productRepository = new ProductRepository(context1);
 
-        productRepository.save(new Product(id, "Water", "The best thing to drink"));
+        UUID toRemoveId = UUID.randomUUID();
+        productRepository.save(new Product(UUID.randomUUID(), "Water", "The best thing to drink"));
+        productRepository.save(new Product(toRemoveId, "Bread", "Something to start with"));
+        productRepository.save(new Product(UUID.randomUUID(), "Carrot", "Because they said you will see better"));
 
-        Product found = productRepository.findById(id);
-        System.out.println(found);
+        context1.close();
 
-        entityManager1.close();
+        nextContext();
+        EntityManager context2 = entityManagerFactory.createEntityManager();
+        productRepository = new ProductRepository(context2);
 
+        Product found = productRepository.findById(toRemoveId);
+        context2.getTransaction().begin();
+        context2.remove(found);
+        context2.getTransaction().commit();
+
+        nextContext();
+
+        EntityManager lastContext = entityManagerFactory.createEntityManager();
+        productRepository = new ProductRepository(lastContext);
+
+        List<Product> products = lastContext.createQuery("SELECT p FROM Product p").getResultList();
+        products.forEach(System.out::println);
+
+        lastContext.close();
+    }
+
+    private static void nextContext() {
         System.out.println("-----------------------------");
         System.out.println("-------- NEW CONTEXT --------");
         System.out.println("-----------------------------");
-
-        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
-        productRepository = new ProductRepository(entityManager2);
-
-        System.out.println(productRepository.findById(id));
-
-        entityManager2.close();
     }
 }
