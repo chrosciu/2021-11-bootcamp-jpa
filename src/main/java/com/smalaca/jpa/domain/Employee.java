8d41a2dfdbcb12976076af3238d0c2b4721cc43b
smalaca <ab13.krakow@gmail.com>
2021-11-21 15:31:41 +0100

    add inheritance with strategy one table for all entities

--- src/main/java/com/smalaca/jpa/domain/Employee.java
+++ src/main/java/com/smalaca/jpa/domain/Employee.java
@@ -1,10 +1,12 @@
 package com.smalaca.jpa.domain;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import java.time.LocalDate;
 
 @Entity
+@DiscriminatorValue("EMPLOYEE")
 public class Employee extends Person {
     @Column
     private LocalDate workStartDate;
--- src/main/java/com/smalaca/jpa/domain/User.java
+++ src/main/java/com/smalaca/jpa/domain/User.java
@@ -1,9 +1,11 @@
 package com.smalaca.jpa.domain;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("USER")
 public class User extends Person {
     @Column
     private long currentSessionLength;
