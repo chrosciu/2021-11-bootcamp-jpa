41e231806af16764f84f0af02e5b3078c8879068
Marcin Chrost <mchrost82@gmail.com>
2021-12-09 16:56:30 +0100

    stored procedures

---
+++ src/main/java/com/smalaca/jpa/StoredProcedures.java
@@ -0,0 +1,20 @@
+package com.smalaca.jpa;
+
+import com.smalaca.jpa.utils.DbPopulator;
+import com.smalaca.jpa.utils.DbUtils;
+import com.smalaca.jpa.utils.StoredProceduresLoader;
+
+public class StoredProcedures {
+    public static void main(String[] args) {
+        DbUtils.runInEntityManagerFactory(entityManagerFactory -> {
+            StoredProceduresLoader.load(entityManagerFactory);
+            DbPopulator.populateDb(entityManagerFactory);
+            DbUtils.runInEntityManagerContext(entityManagerFactory, context -> {
+                var nativeQuery = context.createNativeQuery("CALL GET_INVOICE_ITEMS_COUNT(:minAmount)");
+                nativeQuery.setParameter("minAmount", 3);
+                var result = (Integer)(nativeQuery.getSingleResult());
+                System.out.println(result);
+            });
+        });
+    }
+}
