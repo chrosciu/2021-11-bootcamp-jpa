b8e5681494e5dc4dc9c748eb7fd081241dbebc4f
Marcin Chrost <mchrost82@gmail.com>
2022-05-06 11:53:37 +0200

    refactored DbPopulator and JpaQueries

--- src/main/java/com/smalaca/jpa/JpaQueries.java
+++ src/main/java/com/smalaca/jpa/JpaQueries.java
@@ -4,10 +4,12 @@ import com.smalaca.jpa.utils.DbPopulator;
 import com.smalaca.jpa.utils.DbUtils;
 
 public class JpaQueries {
-    public static void main(String[] args) {
-        DbUtils.runInEntityManagerFactory(entityManagerFactory -> {
-            DbPopulator.populateDb(entityManagerFactory);
-            DbPopulator.displayAll(entityManagerFactory);
-        });
+    private static class DisplayAll {
+        public static void main(String[] args) {
+            DbUtils.runInEntityManagerFactory(entityManagerFactory -> {
+                DbUtils.runInEntityManagerContext(entityManagerFactory, DbPopulator::populateDb);
+                DbUtils.runInEntityManagerContext(entityManagerFactory, DbPopulator::displayAll);
+            });
+        }
     }
 }
