105489d47979acf20f7653d295b622edc388c466
Marcin Chrost <mchrost82@gmail.com>
2022-05-14 14:25:33 +0200

    task 2 preparation

---
+++ src/main/java/com/smalaca/jpa/Task2.java
@@ -0,0 +1,24 @@
+package com.smalaca.jpa;
+
+import com.smalaca.jpa.domain.InvoiceStatus;
+import com.smalaca.jpa.dto.InvoiceDto;
+import com.smalaca.jpa.utils.DbPopulator;
+import com.smalaca.jpa.utils.DbUtils;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class Task2 {
+    public static void main(String[] args) {
+        DbUtils.runInEntityManagerFactory(entityManagerFactory -> {
+            DbUtils.runInEntityManagerContext(entityManagerFactory, DbPopulator::populateDb);
+            DbUtils.runInEntityManagerContext(entityManagerFactory, context -> {
+                System.out.println(findInvoicesWithSQL(context, "E", InvoiceStatus.CREATED));
+            });
+        });
+    }
+
+    private static List<InvoiceDto> findInvoicesWithSQL(EntityManager context, String offerNumberPattern, InvoiceStatus status) {
+        return null; //TODO
+    }
+}
