525e14583a1a8433621d4e128010addb0587270e
Marcin Chrost <mchrost82@gmail.com>
2022-05-14 10:34:45 +0200

    jpa queries - part 1

--- src/main/java/com/smalaca/jpa/JpaQueries.java
+++ src/main/java/com/smalaca/jpa/JpaQueries.java
@@ -1,5 +1,7 @@
 package com.smalaca.jpa;
 
+import com.smalaca.jpa.domain.Invoice;
+import com.smalaca.jpa.domain.InvoiceStatus;
 import com.smalaca.jpa.utils.DbPopulator;
 import com.smalaca.jpa.utils.DbUtils;
 
@@ -9,6 +11,46 @@ public class JpaQueries {
             DbUtils.runInEntityManagerFactory(entityManagerFactory -> {
                 DbUtils.runInEntityManagerContext(entityManagerFactory, DbPopulator::populateDb);
                 DbUtils.runInEntityManagerContext(entityManagerFactory, DbPopulator::displayAll);
+            });
+        }
+    }
+
+    private static class InvoiceById {
+        public static void main(String[] args) {
+            DbUtils.runInEntityManagerFactory(entityManagerFactory -> {
+                DbUtils.runInEntityManagerContext(entityManagerFactory, DbPopulator::populateDb);
+                DbUtils.runInEntityManagerContext(entityManagerFactory, context -> {
+                    var invoice = context.find(Invoice.class, DbPopulator.getInvoiceId());
+                    System.out.println("Invoice by id(" + DbPopulator.getInvoiceId() + ") : " + invoice);
+                });
+            });
+        }
+    }
+
+    private static class AllInvoices {
+        public static void main(String[] args) {
+            DbUtils.runInEntityManagerFactory(entityManagerFactory -> {
+                DbUtils.runInEntityManagerContext(entityManagerFactory, DbPopulator::populateDb);
+                DbUtils.runInEntityManagerContext(entityManagerFactory, context -> {
+                    var queryString = "from Invoice i";
+                    var query = context.createQuery(queryString, Invoice.class);
+                    var result = query.getResultList();
+                    System.out.println("All invoices: " + result);
+                });
+            });
+        }
+    }
+
+    private static class AllInvoicesStatuses {
+        public static void main(String[] args) {
+            DbUtils.runInEntityManagerFactory(entityManagerFactory -> {
+                DbUtils.runInEntityManagerContext(entityManagerFactory, DbPopulator::populateDb);
+                DbUtils.runInEntityManagerContext(entityManagerFactory, context -> {
+                    var queryString = "select i.status from Invoice i";
+                    var query = context.createQuery(queryString, InvoiceStatus.class);
+                    var result = query.getResultList();
+                    System.out.println("All invoices statuses: " + result);
+                });
             });
         }
     }
